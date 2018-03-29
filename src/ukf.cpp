c521ac039049e175bd006acc87e5d9cbfa3470e2
Ganesh Prabakaran <Ganesh_Prabakaran@ad.Infosys.com>
2018-03-29 17:13:06 +0530

    paramter change in UKF

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -27,7 +27,7 @@ UKF::UKF() {
   std_a_ = 2;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = .2;
+  std_yawdd_ = .3;
 
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
